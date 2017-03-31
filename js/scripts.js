beebde24ca312c8024113a9661239a67f9e0753c
Glen <glen03xs@gmail.com>
2017-03-31 14:24:39 +0800

    before minor reformat01

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,23 @@
  */
 
 $(document).ready(function() {
-
+    $("#main-slides").owlCarousel({
+    
+    items : 5,
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 10,
+    paginationSpeed : 10,
+    singleItem:true,
+    autoPlay: true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
 });
 
 
